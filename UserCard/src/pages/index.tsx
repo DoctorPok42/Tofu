6cb1a7c6100dd6e781d2bb8b9befc5ae667a3a5f
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2023-10-02 10:14:43 +0200

    init commit UserPage
    add the mvp version of userpage

---
+++ UserCard/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Tofu</title>
+        <meta name="description" content="Tofu" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container">
+
+      </main>
+    </>
+  )
+}
+
+export default Home
